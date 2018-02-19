217ccdf2e3c7c13c4e97a16eeffc58f18e784bde
Dimitri Walters <me@dimi.io>
2018-02-19 13:37:00 -0500

    Add bar chart from chartjs to page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+var BarChart = require("react-chartjs").Bar;
+var data = {
+  labels: ["Si", "Se", "Ni", "Ne", "Fi", "Fe", "Ti", "Te"],
+  datasets: [
+    {
+      fillColor: "rgba(220,220,220,0.5)",
+      strokeColor: "rgba(220,220,220,0.8)",
+      highlightFill: "rgba(220,220,220,0.75)",
+      highlightStroke: "rgba(220,220,220,1)",
+      data: [50, 50, 50, 50, 50, 50, 50, 50]
+    }
+  ]
+};
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>MBTI</h1>
+        <BarChart data={data} width="778" height="389" responsive="true" />
       </div>
     );
   }
