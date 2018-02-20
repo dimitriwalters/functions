895c70923271076032793e68777359cf2499551b
Dimitri Walters <me@dimi.io>
2018-02-19 23:08:44 -0500

    Add a few sliders to the page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import Slider from 'material-ui/Slider';
 import './App.css';
 
 var BarChart = require("react-chartjs").Bar;
@@ -15,12 +17,35 @@ var data = {
   ]
 };
 
+class LabeledSlider extends Component {
+  render() {
+    return (
+      <div>
+        <span>Label</span>
+        <Slider style={{width: 200}} />
+      </div>
+    )
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>MBTI</h1>
         <BarChart data={data} width="778" height="389" responsive="true" />
+        <MuiThemeProvider>
+          <div style={{display: 'flex', justifyContent: 'center'}}>
+            <div>
+              <LabeledSlider />
+              <LabeledSlider />
+            </div>
+            <div>
+              <LabeledSlider />
+              <LabeledSlider />
+            </div>
+          </div>
+        </MuiThemeProvider>
       </div>
     );
   }
